b23e34699b0bf3a1aced518e874bca533d7602e6
mzaini30 <muhzaini30@gmail.com>
2023-03-15 17:44:42 +0800

    simpan dulu

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,1 +1,18 @@
-<div class="w-full min-h-screen bg-green-200">a</div>
+<div class="w-full min-h-screen bg-green-200 p-5 grid grid-cols-3 gap-5">
+  <div>
+    <div>Quran Page Number</div>
+    <textarea
+      name=""
+      class="form-control w-full p-2 focus:outline-none"
+      id=""
+      cols="30"
+      rows="10"
+    />
+  </div>
+  <div>
+    <div>Juz</div>
+  </div>
+  <div>
+    <div>Surat</div>
+  </div>
+</div>
